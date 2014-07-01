75257b01c137124be602ce297a6f49562b186014
DJ <jieding@gmail.com>
2014-07-01 12:05:40 -0400

    Added user friendships and associated tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
     user = User.new
     assert !user.save
@@ -39,10 +40,28 @@ class UserTest < ActiveSupport::TestCase
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
 
-  test "a user can have a correctly formatted profile name" do
-    user = User.new(first_name: 'J', last_name: 'D', email: 'djrlthu@gmail.com')
-    user.password = user.password_confirmation = 'kktcondition'
-    user.profile_name = 'JD_1J'
-    assert user.valid?
-  end
+  # test "a user can have a correctly formatted profile name" do
+  #   user = User.new(first_name: 'J', last_name: 'D', email: 'djrlthu@gmail.com')
+  #   user.password = user.password_confirmation = 'kktcondition'
+  #   user.profile_name = 'JD_1J'
+  #   assert user.valid?
+  # end
+
+    test "that no error is raised when trying to access a friend list" do
+      assert_nothing_raised do
+        users(:jd).friends
+      end
+    end
+
+
+    test "that creating friendships on a user works" do
+      users(:jd).friends << users(:zz)
+      users(:jd).friends.reload
+      assert users(:jd).friends.include?(users(:zz))
+    end
+
+    test "that creating a friendship based on user id and friend id works" do
+      UserFriendship.create user_id: users(:jd).id, friend_id: users(:zz).id
+      assert users(:jd).friends.include?(users(:zz))
+    end
 end
